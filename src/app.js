fd0cd0166746cff7c4d3faae0da13833fc7c69be
tsunodajapa <gabrieltsunoda@gmail.com>
2022-07-12 20:48:33 -0300

    feature: organizando projeto

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const { request } = require('express');
+const express = require('express');
+
+const mainRouter = require('./routes/main');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/', mainRouter);
+
+app.listen(3333);
+
